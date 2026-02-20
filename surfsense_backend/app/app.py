78a0e4f2884a333422176304d7fb887f27881e78
arsalanijaz <arsalanijaz@gmail.com>
2026-02-21 00:33:09 +0500

    Update app.py for CORS

--- surfsense_backend/app/app.py
+++ surfsense_backend/app/app.py
@@ -252,6 +252,8 @@ if not config.BACKEND_URL or (
         [
             "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "http://localhost:3000",
+        "https://shimmering-surprise-production.up.railway.app",
         ]
     )
 
